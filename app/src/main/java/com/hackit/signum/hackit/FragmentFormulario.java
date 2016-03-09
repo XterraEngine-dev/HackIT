304d823129f44f3ac6f716a0986597c0677dc432
Brim <brim.carrera@gmail.com>
2016-03-09 01:46:34 -0600

    Ola ke ase? -Brim

--- app/src/main/java/com/hackit/signum/hackit/FragmentFormulario.java
+++ app/src/main/java/com/hackit/signum/hackit/FragmentFormulario.java
@@ -3,10 +3,22 @@ package com.hackit.signum.hackit;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.yalantis.starwars.TilesFrameLayout;
+import com.yalantis.starwars.interfaces.TilesFrameLayoutListener;
+
+import org.w3c.dom.Text;
 
 
 /**
@@ -15,9 +27,13 @@ import android.view.ViewGroup;
  * {@link FragmentFormulario.OnFragmentInteractionListener} interface
  * to handle interaction events.
  */
-public class FragmentFormulario extends Fragment {
+public class FragmentFormulario extends Fragment implements TilesFrameLayoutListener {
 
     private OnFragmentInteractionListener mListener;
+
+    private TilesFrameLayout mTilesFrameLayout;
+    private EditText etCodigo;
+    private TextInputLayout tilCodigo;
 
     public FragmentFormulario() {
         // Required empty public constructor
@@ -27,8 +43,24 @@ public class FragmentFormulario extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_formulario, container, false);
+        View vista = inflater.inflate(R.layout.fragment_formulario, container, false);
+        mTilesFrameLayout = (TilesFrameLayout) vista.findViewById(R.id.tiles_frame_layout);
+        mTilesFrameLayout.setOnAnimationFinishedListener(this);
+        etCodigo = (EditText) vista.findViewById(R.id.etCodigo);
+        tilCodigo = (TextInputLayout) vista.findViewById(R.id.tilCodigo);
+        vista.findViewById(R.id.bComprobar).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (etCodigo.getText().toString().trim().equals(getResources().getString(Params.CODAHACK))) {
+                    mTilesFrameLayout.startAnimation();
+                } else {
+                    tilCodigo.setError(getString(R.string.error_codigo));
+                    etCodigo.setText("");
+                }
+            }
+        });
+        return vista;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -55,6 +87,17 @@ public class FragmentFormulario extends Fragment {
         mListener = null;
     }
 
+    @Override
+    public void onAnimationFinished() {
+        getActivity().findViewById(R.id.tvEstado).setVisibility(View.VISIBLE);
+        TextView tvEstado = (TextView) getActivity().findViewById(R.id.tvEstado);
+        Animation myFadeInAnimation = AnimationUtils.loadAnimation(getContext(), R.anim.blink);
+        tvEstado.setText(getString(R.string.codigo_correcto));
+        tvEstado.startAnimation(myFadeInAnimation);
+
+
+    }
+
     /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
@@ -68,5 +111,17 @@ public class FragmentFormulario extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mTilesFrameLayout.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mTilesFrameLayout.onPause();
     }
 }
--- app/src/main/java/com/hackit/signum/hackit/MainActivity.java
+++ app/src/main/java/com/hackit/signum/hackit/MainActivity.java
@@ -1,66 +1,33 @@
 package com.hackit.signum.hackit;
 
-import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.support.v4.app.FragmentActivity;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class MainActivity extends Activity implements View.OnClickListener{
+public class MainActivity extends FragmentActivity implements FragmentFormulario.OnFragmentInteractionListener {
 
     Button boton;
     EditText texto;
     String codigoSecreto;
     TextView resultado;
-    public static String CODAHACK="hACK3d)(24!XS!gNum";
-//olol
+
+
+    //olol
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        boton= (Button) findViewById(R.id.button);
-        texto= (EditText) findViewById(R.id.editText);
-        resultado= (TextView) findViewById(R.id.textView2);
-
-
-        boton.setOnClickListener(this);
-
-
-
-
-
     }
 
 
     @Override
-    public void onClick(View v) {
-
-        codigoSecreto=texto.getText().toString();
-
-        if (codigoSecreto.equals(CODAHACK)){
-
-            resultado.setText("Código correcto!!!");
-
-
-        }else{
-
-            resultado.setText("Incorrecto!!!");
-
-        }
-
-
-
-
-
+    public void onFragmentInteraction(Uri uri) {
 
     }
 }
